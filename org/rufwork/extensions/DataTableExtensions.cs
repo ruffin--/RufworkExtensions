c193eab0361994ef94baddfb91f1a4cb3a74c578
Ruffin Bailey <git@rufwork.com>
2015-08-02 12:55:59 -0400

    Added DataTableExtensions and SelectTopNRows method there.
    Not sure this is the most efficient way to return a new DataTable with the first X rows from another, but it probably helps that it's just a TOP rather than a WHERE-ing. That is, it's not too inefficient to grab the top rows, but I wouldn't want it to get much more complicated than this. Seems most use cases would be better using a DataView or just a subset of Rows from the original table, since grabbing the TOP N and putting them into another table is almost always going to be overkill from an efficiency standpoint. /shrug

---
+++ org/rufwork/extensions/DataTableExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace org.rufwork.extensions
+{
+    public static class DataTableExtensions
+    {
+        /// <summary>
+        /// This method will take the original DataTable and return another
+        /// DataTable with the as-specified first `n` rows from the original in it.
+        /// Note that the original DataTable needs to, obviously, already be sorted.
+        /// TODO: There's got to be a better way to do this, especially if we weren't
+        /// simply taking the first N, but WHERE-ing somehow.
+        /// </summary>
+        /// <param name="t">The `this` Datatable</param>
+        /// <param name="n">The number of rows to take.</param>
+        /// <returns></returns>
+        public static DataTable SelectTopNRows(this DataTable t, int n)
+        {
+            DataRow[] aRows = t.Select("");
+            DataTable tableRet = t.Clone();
+
+            for (int i = 0; i < aRows.Length; i++)
+            {
+                if (i >= n) break;
+                tableRet.Rows.Add(aRows[i].ItemArray);
+            }
+
+            return tableRet;
+        }
+    }
+}
