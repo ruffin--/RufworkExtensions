a95fd3d42c83bccaaf1c8aaf5ccaad8228ab9e55
Ruffin Bailey <git@rufwork.com>
2015-09-16 17:44:32 -0400

    Convenience extension for DateTime's ToString("yyyy-MM-dd HH:mm:ss.fff")

---
+++ org/rufwork/extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace org.rufwork.extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToRufString(this DateTime dt)
+        {
+            return dt.ToString("yyyy-MM-dd HH:mm:ss.fff");
+        }
+    }
+}
